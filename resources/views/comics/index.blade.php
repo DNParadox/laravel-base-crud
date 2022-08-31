06682ee02aab2359fb9172bcd710084b37cec716
DNParadox <m96francesco@gmail.com>
2022-08-31 17:18:12 +0200

    Esercizio completato

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,11 +5,30 @@
 
    @foreach ($comics as $comic)
    <div>
-        <div> <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">Title : {{ $comic->title }}</a> </div>
+        <div> Title : {{ $comic->title }} </div>
+        
         <div> Type : {{ $comic->type }}</div>
-        <div> <a href="{{ route('comics.show', ['comic' => $comic->id]) }}"> dettagli fumetto</a></div>
-   
+
+      
+        <div>
+             <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">Dettagli fumetto</a>
+        </div>
+
+        <div>
+            <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica Fumetto</a>
+        </div>
+
+        <div>
+            <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="post">
+                @csrf
+                @method('DELETE')
+                {{-- onClick: Al Click, chiederà se vo  --}}
+                <input type="submit" value="Cancella" onClick="return confirm('Sei sicuro di voler cancellare?');">
+            </form>
+        </div>
     </div>
+
+    <br>
        
    @endforeach
 @endsection
